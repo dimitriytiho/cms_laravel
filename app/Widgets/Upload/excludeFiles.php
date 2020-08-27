1e0a32111c185708616d9b62d974470725327242
«dimitriyyuliya» <dimitriyyuliya@gmail.com>
2020-08-27 20:05:06 +0300

    Добавлены комментарии для класса Upload.

--- app/Widgets/Upload/excludeFiles.php
+++ app/Widgets/Upload/excludeFiles.php
@@ -2,10 +2,18 @@
 
 /*
  * Здесь указать те папки или файлы, которые не нужно обновлять.
+ *
+ *
+ * Папки или файлы указаные по-умолчанию в классе:
+ * app/Widgets/Upload
+ * app/Modules/Admin/add_routes.php
+ * app/Modules/Admin/Nav.php
+ *
  */
 
 return [
 
+	// Modules
     'app/Modules/lang/vendor/backup',
     'app/Modules/Page',
 
